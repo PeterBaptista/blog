334a27bff4e54f07272ac0f32c66366b97cbb671
Pedro Baptista <pedrolk2012@gmail.com>
2023-05-06 18:48:23 -0300

    added the main functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
+  root "articles#index"
 
+  resources :articles do
+    resource :comments
   # Defines the root path route ("/")
   # root "articles#index"
 end
